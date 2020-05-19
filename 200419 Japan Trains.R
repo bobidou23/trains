f4224f4e33bd72676dc3c760407e8567faa7d9ac
bobidou23 <kshiromoto0723@gmail.com>
2020-05-19 23:52:44 +0900

    first draft JR

--- 200419 Japan Trains.R
+++ 200419 Japan Trains.R
@@ -1,6 +1,6 @@
 install.packages("kokudosuuchi")
 library(pacman)
-p_load(kokudosuuchi,rvest,tidyverse)
+p_load(kokudosuuchi,rvest,tidyverse,sf)
 
 ja_trains <- getKSJURL("N02", fiscalyear=2018) %>%
   select(zipFileUrl) %>%
@@ -9,12 +9,25 @@ ja_trains <- getKSJURL("N02", fiscalyear=2018) %>%
 ja_lines <- ja_trains$`N02-18_RailroadSection`
 ja_stations <- ja_trains$`N02-18_Station`
 rm(ja_trains)
+
+# st_write(ja_lines, "ja_lines.shp", layer_options = "ENCODING=UTF-8")
+# ja_lines <- st_read("~/Desktop/Datasets/ja_lines.shp")
+# ja_stations <- st_read("~/Desktop/Datasets/ja_stations.shp")
 colnames(ja_lines) <- c("railroad_type","operator_type","line","operator","geometry")
 colnames(ja_stations) <- c("railroad_type","operator_type","line","operator","station","geometry")
 
 ja_lines %>%
-  filter(operator=="福岡市") %>%
-  mutate(number=row_number()) %>%
-  ggplot(aes(colour=number)) +
+  filter(str_sub(operator,start=-4)=="旅客鉄道") %>%
+  mutate(operator = paste0("JR",str_sub(operator,end=-5)),
+         type = case_when(str_sub(line, start=-3)=="新幹線"~"新幹線", TRUE~"在来線")) %>%
+  group_by(line, operator, type) %>%
+  summarise(geometry = st_union(geometry)) %>%
+  ggplot(aes(colour=operator,size=type,alpha=type)) +
   geom_sf() +
-  scale_color_distiller(palette="Dark2")
+  scale_size_manual(values = c("新幹線"=1.5,"在来線"=0.5)) +
+  scale_alpha_manual(values = c("新幹線"=.7,"在来線"=0.4)) +
+  scale_color_manual(values = jr_colours)
+
+jr_colours <- c("JR九州"="#ff0200", "JR北海道"="#2cb431",
+                "JR四国"="#1ba5ca", "JR東日本"="#0b8e0f",
+                "JR東海"="#f77421", "JR西日本"="#0173ca")
